b042f0d16cd0d3518adef34c486cbbd71b19af0f
Vansh-Panchal <vansh.panchal719@gmail.com>
2024-05-29 16:04:33 +0530

    Specific Inventory Fragment and API_1

---
+++ app/src/main/java/com/example/ims/M_product.kt
@@ -0,0 +1,3 @@
+package com.example.ims
+
+data class M_product(val name:String,val quantity : Int,val price : Int)
---
+++ app/src/main/java/com/example/ims/MyData.kt
@@ -0,0 +1,3 @@
+package com.example.ims
+
+data class MyData (val key1: String,val key2 :Int)
