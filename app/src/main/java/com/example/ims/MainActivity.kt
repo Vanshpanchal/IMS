c6e3a97c373d011ce1f2114adbdd8e6487b10dda
Vansh-Panchal <vansh.panchal719@gmail.com>
2024-06-03 11:09:36 +0530

    API End

--- app/src/main/java/com/example/ims/MainActivity.kt
+++ app/src/main/java/com/example/ims/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.ims
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
@@ -14,6 +17,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var bind: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -101,6 +105,7 @@ class MainActivity : AppCompatActivity() {
 //            "inventory" -> bind.bottomNavigation.selectedItemId = R.id.item_2
 //        }
 //    }
+
 
 }
 
--- app/src/main/java/com/example/ims/specific_inventory.kt
+++ app/src/main/java/com/example/ims/specific_inventory.kt
@@ -1,6 +1,8 @@
 package com.example.ims
 
 import android.app.Activity
+import android.app.Activity.RESULT_OK
+import android.content.ContentResolver
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
@@ -11,7 +13,9 @@ import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.graphics.drawable.VectorDrawable
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -20,11 +24,18 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
 import androidx.compose.ui.text.input.KeyboardType.Companion.Uri
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.checkSelfPermission
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.util.query
 import com.android.volley.AuthFailureError
 import com.android.volley.Request
 import com.android.volley.Response
@@ -38,10 +49,13 @@ import com.google.gson.reflect.TypeToken
 import org.json.JSONException
 import org.json.JSONObject
 import java.io.ByteArrayOutputStream
+import kotlin.math.log
 
 //import retrofit2.Response
 
 class specific_inventory : Fragment() {
+
+
     lateinit var previewDialog: BottomSheetDialog
     lateinit var binding: FragmentSpecificInventoryBinding
     lateinit var product: ArrayList<inv_itemsItem>
@@ -49,7 +63,17 @@ class specific_inventory : Fragment() {
     private lateinit var sharedPreferences: SharedPreferences
     private lateinit var editor: SharedPreferences.Editor
     var imageView: Uri? = null
+    private var product_img: ImageView? = null
 
+    private var galleryLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK && result.data != null) {
+//                    galleryprofile()
+                product_img?.setImageURI(result.data!!.data)
+                imageView = result.data!!.data!!
+                Log.d("TAG", "result:${result.data!!.data} ")
+            }
+        }
 
     override fun onCreateView(
 
@@ -70,13 +94,6 @@ class specific_inventory : Fragment() {
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         previewDialog = BottomSheetDialog(requireContext())
         product = arrayListOf()
-//        val data = product_list("Product 1", "Inventory 1", "4")
-//        product.add(data)
-//        product.add(data)
-//        product.add(data)
-//        product.add(data)
-
-//        load_data()
         sharedPreferences =
             requireContext().getSharedPreferences("USERDATA", AppCompatActivity.MODE_PRIVATE)
         editor = sharedPreferences.edit()
@@ -126,43 +143,13 @@ class specific_inventory : Fragment() {
             )
                 .setView(view)
                 .create()
-            val product_img = view.findViewById<ImageView>(R.id.P_img)
+            product_img = view.findViewById<ImageView>(R.id.P_img)
             view.findViewById<ImageView>(R.id.P_img).setOnClickListener {
-                openGallery()
-//                product_img.setImageURI(ImageView!!)
+                requestpermission()
             }
             view.findViewById<Button>(R.id.submit_btn).setOnClickListener {
-                val url = "http://10.0.2.2:8000/items/add"
-                Log.d("hello", "itemClickListener: $} ")
-//                val img = drawableToByteArray(R.drawable.img2)
-                val bodyParams = JSONObject().apply {
-                    put("itemName", "Shoes")
-                    put("itemId", "itm01")
-                    put("pricePerUnit", "100")
-                    put("stock", "10")
-                    put("inventoryId", "05")
-                    put("category", "Shoes")
-                    put("itemimage",imageView)
-                }
-                val params = mapOf(
-                    "inventoryId___" to inv_id
-                )
-                val getRequest = CustomReq1(
-                    url,
-                    access_token!!,
-                    params,
-                    bodyParams,
-                    { response ->
-
-                        Log.d("API_CHECK", "Response: ${response}")
-
-                    },
-                    { error ->
-                        Log.d("API_CHECK", "Error: ${String(error.networkResponse.data)}")
-                    }
-                )
-
-                Volley.newRequestQueue(requireContext()).add(getRequest)
+                Log.d("TAG", "result:${imageView} ")
+                add_product(inv_id, access_token, imageView!!)
                 dialog.dismiss()
 
             }
@@ -170,29 +157,6 @@ class specific_inventory : Fragment() {
         }
     }
 
-    fun drawableToByteArray(context: Context, drawableId: Int): ByteArray {
-        val drawable = context.getDrawable(drawableId)
-        val bitmap = when (drawable) {
-            is BitmapDrawable -> drawable.bitmap
-            is VectorDrawable -> getBitmapFromVectorDrawable(drawable)
-            else -> throw IllegalArgumentException("Unsupported drawable type")
-        }
-        val stream = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
-        return stream.toByteArray()
-    }
-
-    private fun getBitmapFromVectorDrawable(vectorDrawable: VectorDrawable): Bitmap {
-        val bitmap = Bitmap.createBitmap(
-            vectorDrawable.intrinsicWidth,
-            vectorDrawable.intrinsicHeight,
-            Bitmap.Config.ARGB_8888
-        )
-        val canvas = Canvas(bitmap)
-        vectorDrawable.setBounds(0, 0, canvas.width, canvas.height)
-        vectorDrawable.draw(canvas)
-        return bitmap
-    }
 
     fun load_data() {
         val specific_inv_adapter = specific_inv_adapter(product)
@@ -225,21 +189,104 @@ class specific_inventory : Fragment() {
         })
     }
 
-    private fun openGallery() {
-        val intent = Intent(Intent.ACTION_PICK)
-        intent.type = "image/*"
-        startActivityForResult(intent, 50)
+    private fun checkpermissionRead() = ActivityCompat.checkSelfPermission(
+        requireContext(), android.Manifest.permission.READ_EXTERNAL_STORAGE
+    ) == PackageManager.PERMISSION_GRANTED
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun checkpermissionReadImages() = ActivityCompat.checkSelfPermission(
+        requireContext(), android.Manifest.permission.READ_MEDIA_IMAGES
+    ) == PackageManager.PERMISSION_GRANTED
+
+    private fun requestpermission() {
+        val permissiontoRequest = mutableListOf<String>()
+
+        if (Build.VERSION.SDK_INT >= 33) {
+            if (!checkpermissionReadImages()) {
+                permissiontoRequest.add(android.Manifest.permission.READ_MEDIA_IMAGES)
+            } else {
+                val galleryIntent =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                galleryLauncher.launch(galleryIntent)
+            }
+
+            if (permissiontoRequest.isNotEmpty()) {
+                ActivityCompat.requestPermissions(
+                    requireContext() as Activity,
+                    permissiontoRequest.toTypedArray(),
+                    0
+                )
+            }
+        } else {
+            if (!checkpermissionRead()) {
+                permissiontoRequest.add(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            } else {
+                val galleryIntent =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                galleryLauncher.launch(galleryIntent)
+////                profileImage()
+//                profile()
+            }
+
+            if (permissiontoRequest.isNotEmpty()) {
+                ActivityCompat.requestPermissions(
+                    requireContext() as Activity,
+                    permissiontoRequest.toTypedArray(),
+                    0
+                )
+            }
+        }
+
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 50 && resultCode == Activity.RESULT_OK) {
-            imageView= data?.data
-//            imageView = selectedImageUri
-//            binding.setImageURI(selectedImageUri)
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 0 && grantResults.isNotEmpty()) {
+            for (i in grantResults.indices) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d("hello", "onRequestPermissionsResult: Done")
+
+                }
+            }
         }
     }
 
+    fun add_product(inv_id: String, access_token: String ,imageView: Uri) {
+        val url = "http://10.0.2.2:8000/items/add"
+        Log.d("hello", "itemClickListener: ${imageView}} ")
+//                val img = drawableToByteArray(R.drawable.img2)
+        val bodyParams = JSONObject().apply {
+            put("itemName", "Shoes")
+            put("itemId", "itm01")
+            put("pricePerUnit", "100")
+            put("stock", "10")
+            put("inventoryId", "05")
+            put("category", "Shoes")
+            put("itemimage", imageView)
+        }
+        val params = mapOf(
+            "inventoryId___" to inv_id
+
+        )
+        val getRequest = CustomReq1(
+            url,
+            access_token!!,
+            params,
+            bodyParams,
+            { response ->
+
+                Log.d("API_CHECK", "Response: ${response}")
+
+            },
+            { error ->
+                Log.d("API_CHECK", "Error: ${String(error.networkResponse.data)}")
+            }
+        )
+
+        Volley.newRequestQueue(requireContext()).add(getRequest)
+    }
 
 }
 
