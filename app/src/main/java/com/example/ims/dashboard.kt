4a02a077d7b1e1ea05b4618c3052fde6654e33c8
Vansh-Panchal <vansh.panchal719@gmail.com>
2024-06-10 15:47:04 +0530

    Logic Implementation in Dashboard Fragment

--- app/src/main/java/com/example/ims/dashboard.kt
+++ app/src/main/java/com/example/ims/dashboard.kt
@@ -13,6 +13,9 @@ import android.widget.ListView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ims.databinding.FragmentDashboardBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import okhttp3.Callback
 import retrofit2.Call
 import retrofit2.Response
@@ -20,13 +23,20 @@ import retrofit2.Response
 
 class dashboard : Fragment() {
     lateinit var binding: FragmentDashboardBinding
+    lateinit var auth: FirebaseAuth
+    lateinit var fs: FirebaseFirestore
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
         binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        val msg = arrayOf("Packages to be packed", "Packages to be shipped", "Packages to be delivered", "Packages to be Invoiced")
+        val msg = arrayOf(
+            "Packages to be packed",
+            "Packages to be shipped",
+            "Packages to be delivered",
+            "Packages to be Invoiced"
+        )
         val num = arrayOf("450", "21", "10", "24")
         val images = arrayOf(
             R.drawable.logistics,
@@ -34,6 +44,9 @@ class dashboard : Fragment() {
             R.drawable.logistics,
             R.drawable.logistics
         )
+        auth = FirebaseAuth.getInstance()
+        fs = FirebaseFirestore.getInstance()
+
         // Adapter
         val adapter = list_view_adapter(requireContext(), R.layout.list_view, msg, num, images)
 
@@ -56,6 +69,29 @@ class dashboard : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        fs.collection("Product").document(auth.currentUser?.uid!!).collection("MyProduct").get()
+            .addOnSuccessListener {
+                binding.totalItems.text = it.size().toString()
+                Log.d("D_CHECK", "onViewCreated: ${it.size()}")
+            }
+
+        fs.collection("Product").document(auth.currentUser?.uid!!).collection("MyProduct")
+            .whereNotEqualTo("LowStock", "-1").get().addOnSuccessListener {
+                binding.totalNotify.text = it.size().toString()
+                Log.d("D_CHECK", "onViewCreated : ${it.size()}")
+                var count = 0
+                for (doc in it) {
+                    Log.d("D_CHECK", "onViewCreated: ${doc.get("LowStock").toString()}")
+                    if (doc.get("Stock").toString().toInt() < doc.get("LowStock").toString()
+                            .toInt()
+                    ) {
+                        count++
+                    }
+                }
+                binding.lowStockItems.text = count.toString()
+
+            }
+
 
     }
 
